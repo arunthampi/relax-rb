b605d20b30f9b0ba06ae8dbf6d168e9716ae7aa8
arunthampi <arun.thampi@gmail.com>
2015-10-10 17:47:07 -0700

    Add logging for EventListener

--- lib/relax/event_listener.rb
+++ lib/relax/event_listener.rb
@@ -2,12 +2,17 @@ module Relax
   class EventsQueueNotSetError < StandardError; end
 
   class EventListener < Base
+    DEFAULT_LOG_LEVEL        = 'info'
+
     @@callback = nil
+    @@logger = nil
 
     def self.listen!
       if relax_events_queue.nil? || relax_events_queue == ""
         raise EventsQueueNotSetError, "Environment Variable RELAX_EVENTS_QUEUE is not set"
       end
+
+      self.log("Listening for Relax Events...")
 
       while true do
         queue_name, event_json = redis.blpop(relax_events_queue)
@@ -19,12 +24,25 @@ module Relax
       end
     end
 
+    def self.log(text, level = DEFAULT_LOG_LEVEL)
+      return if logger.nil?
+      logger.send(level, "#{Time.now.strftime('%FT%T%z')}: #{text}")
+    end
+
     def self.callback=(cb)
       @@callback = cb
     end
 
     def self.callback
       @@callback
+    end
+
+    def self.logger=(logger)
+      @@logger = logger
+    end
+
+    def self.logger
+      @@logger
     end
 
     def self.relax_events_queue
