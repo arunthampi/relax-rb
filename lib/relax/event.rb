8c69f2d5cd4343713899588934875260508c6d68
arunthampi <arun.thampi@gmail.com>
2015-11-23 15:57:44 -0800

    Add #to_hash method to Relax::Event

--- lib/relax/event.rb
+++ lib/relax/event.rb
@@ -1,10 +1,18 @@
 module Relax
   class Event
-    attr_accessor :type, :user_uid, :channel_uid, :team_uid, :im, :text,
-                  :relax_bot_uid, :timestamp, :provider, :event_timestamp
+    ATTRIBUTES = [:type, :user_uid, :channel_uid, :team_uid, :im, :text,
+                  :relax_bot_uid, :timestamp, :provider, :event_timestamp]
+
+    attr_accessor *ATTRIBUTES
 
     def initialize(opts = {})
       opts.each { |k,v| self.send("#{k}=", v) if self.respond_to?("#{k}=") }
+    end
+
+    def to_hash
+      hash = {}
+      ATTRIBUTES.each { |a| hash[a] = self.send(a) }
+      hash
     end
   end
 end
---
+++ spec/relax/event_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Relax::Event do
+  describe '#to_hash' do
+    let!(:event) { Relax::Event.new(
+      type: 'message_new',
+      user_uid: 'user_uid',
+      channel_uid: 'channel_uid',
+      team_uid: 'team_uid',
+      im: true,
+      text: 'hello world',
+      relax_bot_uid: 'URELAXBOT',
+      timestamp: '1000000.1',
+      provider: 'slack',
+      event_timestamp: '1000000.2')
+    }
+
+    it 'should return a hash with all of the attributes' do
+      hash = event.to_hash
+      expect(hash[:type]).to eql 'message_new'
+      expect(hash[:user_uid]).to eql 'user_uid'
+      expect(hash[:channel_uid]).to eql 'channel_uid'
+      expect(hash[:team_uid]).to eql 'team_uid'
+      expect(hash[:im]).to eql true
+      expect(hash[:text]).to eql 'hello world'
+      expect(hash[:relax_bot_uid]).to eql 'URELAXBOT'
+      expect(hash[:timestamp]).to eql '1000000.1'
+      expect(hash[:provider]).to eql 'slack'
+      expect(hash[:event_timestamp]).to eql '1000000.2'
+    end
+  end
+end
