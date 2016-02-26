12a6f320133ed511b3ac756c143093dcdc8460f1
arunthampi <arun.thampi@gmail.com>
2016-02-26 15:27:44 -0800

    Add typing API to Relax::Bot

--- lib/relax/bot.rb
+++ lib/relax/bot.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 module Relax
   class BotsKeyNotSetError < StandardError; end
   class BotsPubsubNotSetError < StandardError; end
@@ -17,6 +19,23 @@ module Relax
           conn.hset(relax_bots_key, team_uid, {team_id: team_uid, token: token}.to_json)
           conn.publish(relax_bots_pubsub, {type: 'team_added', team_id: team_uid}.to_json)
         end
+      end
+    end
+
+    def self.start_typing!(team_uid, channel_uid)
+      if relax_bots_key.nil? || relax_bots_key == ""
+        raise BotsKeyNotSetError, "Environment Variable RELAX_BOTS_KEY is not set"
+      end
+
+      if relax_bots_pubsub.nil? || relax_bots_pubsub == ""
+        raise BotsPubsubNotSetError, "Environment Variable RELAX_BOTS_PUBSUB is not set"
+      end
+
+      message_id = SecureRandom.uuid
+      payload = { id: message_id, type: 'typing', channel: channel_uid }.to_json
+
+      redis.with do |conn|
+        conn.publish(relax_bots_pubsub, {type: 'message', team_id: team_uid, id: message_id, payload: payload}.to_json)
       end
     end
 
